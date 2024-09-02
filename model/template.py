63dfe7e61d2985031b8ff44e576676792fc90755
warren <2016426377@qq.com>
2024-09-02 15:03:54 +0800

    0902

--- model/template.py
+++ model/template.py
@@ -4,12 +4,13 @@ import torch.nn.functional as F
 import torch.nn.init as init
 
 class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, output_channels, kernel_size, padding=0, kernels_per_layer=1):
+    def __init__(self, in_channels, output_channels, kernel_size, stride=1,padding=0, kernels_per_layer=1):
         super().__init__()
         self.depthwise = nn.Conv2d(
             in_channels,
             in_channels * kernels_per_layer,
             kernel_size=kernel_size,
+            stride=stride,
             padding=padding,
             groups=in_channels,
         )
