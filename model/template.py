9eb25aa9dadde314641e613b5bff6207e4c14628
warren <2016426377@qq.com>
2024-10-09 14:15:04 +0800

    add CBAM & SE

--- model/template.py
+++ model/template.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
+"""DSC Usage: DepthwiseSeparableConv(in_channels, out_channels, 3, 1, 1)"""
 class DepthwiseSeparableConv(nn.Module):
     def __init__(self, in_channels, output_channels, kernel_size, stride=1,padding=0, kernels_per_layer=1):
         super().__init__()
@@ -20,7 +21,7 @@ class DepthwiseSeparableConv(nn.Module):
         x = self.depthwise(x)
         x = self.pointwise(x)
         return x
-    
+"""SelfAttention Usage: """   
 class SelfAttentionBlock(nn.Module):
     def __init__(self, in_ch):
         super().__init__()
@@ -51,7 +52,7 @@ class SelfAttentionBlock(nn.Module):
         h = self.proj(h)
         out =  self.gamma+h + x #resnet
         return out
-       
+"""Transformer Encoder Usage:self.transformer=TransformerBottleneck(512,dim_feedforward=1024)"""       
 class TransformerEncoder(nn.Module):
     def __init__(self, in_channels, num_heads=8, dim_feedforward=1024, num_layers=3):
         super(TransformerEncoder, self).__init__()
@@ -85,7 +86,91 @@ class TransformerEncoder(nn.Module):
         x = self.unpatch_embed(x)
         
         return x
+"""CBAM Usage:"""
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.shared_MLP = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        )
+        # self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        # self.relu1 = nn.ReLU()
+        # self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
 
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out =self.shared_MLP(self.avg_pool(x))# self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out =self.shared_MLP(self.max_pool(x))# self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class CBAM(nn.Module):
+    def __init__(self, planes):
+        super(CBAM, self).__init__()
+        self.ca = ChannelAttention(planes)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        x = self.ca(x) * x
+        x = self.sa(x) * x
+        return x
+"""SE Usage: SEAttention(channel=512, reduction=8)"""
+class SEAttention(nn.Module):
+    # 初始化SE模块，channel为通道数，reduction为降维比率
+    def __init__(self, channel=512, reduction=16):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)  # 自适应平均池化层，将特征图的空间维度压缩为1x1
+        self.fc = nn.Sequential(  # 定义两个全连接层作为激励操作，通过降维和升维调整通道重要性
+            nn.Linear(channel, channel // reduction, bias=False),  # 降维，减少参数数量和计算量
+            nn.ReLU(inplace=True),  # ReLU激活函数，引入非线性
+            nn.Linear(channel // reduction, channel, bias=False),  # 升维，恢复到原始通道数
+            nn.Sigmoid()  # Sigmoid激活函数，输出每个通道的重要性系数
+        )
+
+    # 权重初始化方法
+    def init_weights(self):
+        for m in self.modules():  # 遍历模块中的所有子模块
+            if isinstance(m, nn.Conv2d):  # 对于卷积层
+                init.kaiming_normal_(m.weight, mode='fan_out')  # 使用Kaiming初始化方法初始化权重
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)  # 如果有偏置项，则初始化为0
+            elif isinstance(m, nn.BatchNorm2d):  # 对于批归一化层
+                init.constant_(m.weight, 1)  # 权重初始化为1
+                init.constant_(m.bias, 0)  # 偏置初始化为0
+            elif isinstance(m, nn.Linear):  # 对于全连接层
+                init.normal_(m.weight, std=0.001)  # 权重使用正态分布初始化
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)  # 偏置初始化为0
+
+    # 前向传播方法
+    def forward(self, x):
+        b, c, _, _ = x.size()  # 获取输入x的批量大小b和通道数c
+        y = self.avg_pool(x).view(b, c)  # 通过自适应平均池化层后，调整形状以匹配全连接层的输入
+        y = self.fc(y).view(b, c, 1, 1)  # 通过全连接层计算通道重要性，调整形状以匹配原始特征图的形状
+        return x * y.expand_as(x)  # 将通道重要性系数应用到原始特征图上，进行特征重新校准
+   
 class TEMPLATE(nn.Module):
     def __init__(self):
         super(TEMPLATE, self).__init__()
