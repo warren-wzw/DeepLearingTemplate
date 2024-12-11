4bbabcba9aedd74a58e41c3a902f064090c0b791
warren <2016426377@qq.com>
2024-12-11 15:05:00 +0800

    add pid optimizer

--- model/template.py
+++ model/template.py
@@ -86,7 +86,7 @@ class TransformerEncoder(nn.Module):
         x = self.unpatch_embed(x)
         
         return x
-"""CBAM Usage:CBAM(planes=input_channelnum)"""
+"""CBAM Usage:CBAM(planes=input_channel_num)"""
 class ChannelAttention(nn.Module):
     def __init__(self, in_planes, ratio=16):
         super(ChannelAttention, self).__init__()
@@ -131,6 +131,48 @@ class CBAM(nn.Module):
         super(CBAM, self).__init__()
         self.ca = ChannelAttention(planes)
         self.sa = SpatialAttention()
+
+    def forward(self, x):
+        x = self.ca(x) * x
+        x = self.sa(x) * x
+        return x
+"""CSA Usage:CSA(planes=input_channel_num)"""
+class ChannelAttentionEnhancement(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttentionEnhancement, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+           
+        self.fc = nn.Sequential(nn.Conv2d(in_planes, in_planes // 16, 1, bias=False),
+                               nn.ReLU(),
+                               nn.Conv2d(in_planes // 16, in_planes, 1, bias=False))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttentionExtractor(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttentionExtractor, self).__init__()
+
+        self.samconv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.samconv(x)
+        return self.sigmoid(x)
+
+class CSA(nn.Module):
+    def __init__(self, planes):
+        super(CSA, self).__init__()
+        self.ca = ChannelAttentionEnhancement(planes)
+        self.sa = SpatialAttentionExtractor()
 
     def forward(self, x):
         x = self.ca(x) * x
